93a8e16d8aa634b08590961b8ae7d5e3118cc5d1
shrawan949 <shrawanambarte50@gmail.com>
2026-07-04 10:38:10 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.28.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
+
   resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
