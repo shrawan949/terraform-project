e0a5fa2a7fbe1a3f563c1d40eea98ff071f42854
shrawan949 <shrawanambarte50@gmail.com>
2026-07-04 10:40:59 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,20 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 6.28.0"
-    }
-
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
-
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.0"
-    }
-  }
+provider "aws" {
+  region     = "ap-south-1"
+ 
 }
 
   resource "aws_vpc" "myvpc" {
